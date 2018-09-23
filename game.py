c92b33ea6cf54041aa1a5013589d78c2073a3915
Jaehyeok Lee <wogur0310@gmail.com>
2018-09-23 14:44:08 +0900

    game : fix error

--- game.py
+++ game.py
@@ -17,8 +17,8 @@ class Game:
 		for i in range(self.max_state_size):
 			zero_state_black = np.zeros((self.height, self.width))
 			zero_state_white = np.zeros((self.height, self.width))
-			black_states.append(zero_state_black)
-			white_states.append(zero_state_white)
+			self.black_states.append(zero_state_black)
+			self.white_states.append(zero_state_white)
 
 	def play(self, black_player, white_player):
 		self.reset()
@@ -44,7 +44,13 @@ class Game:
 				break
 
 		if winner == 2:
+			print("Draw")
 			return [], [], []
+
+		if winner == 0:
+			print("White Win")
+		else:
+			print("Black Win")
 
 		values = np.zeros(len(current_players))
 		values[np.array(current_players) == winner] = 1.0
@@ -55,12 +61,14 @@ class Game:
 	def get_current_state(self, player):
 		ret = []
 		if player == 1:
-			ret.append(np.ones((height, width)))
+			ret.append(np.ones((self.height, self.width)))
 		else:
-			ret.append(np.zeros((height, width)))
+			ret.append(np.zeros((self.height, self.width)))
 
-		ret.extend(white_states[(len(white_states) - self.max_state_size):])
-		ret.extend(black_states[(len(black_states) - self.max_state_size):])
+		ret.extend(self.white_states[(len(self.white_states) -
+			self.max_state_size):])
+		ret.extend(self.black_states[(len(self.black_states) -
+			self.max_state_size):])
 
 		return ret
 
@@ -90,10 +98,10 @@ class Game:
 	def is_win(self, state, r, c):
 		lo_r = r
 		hi_r = r
-		while state[lo_r][c] == 1 and lo_r > -1:
+		while lo_r > -1 and state[lo_r][c] == 1:
 			lo_r -= 1
 		lo_r += 1
-		while state[hi_r][c] == 1 and hi_r < self.height:
+		while hi_r < self.height and state[hi_r][c] == 1:
 			hi_r += 1
 		hi_r -= 1
 		if hi_r - lo_r + 1 == 5:
@@ -101,10 +109,10 @@ class Game:
 
 		lo_c = c
 		hi_c = c
-		while state[r][lo_c] == 1 and lo_c > -1:
+		while lo_c > -1 and state[r][lo_c] == 1:
 			lo_c -= 1
 		lo_c += 1
-		while state[r][hi_c] == 1 and hi_c < self.width:
+		while hi_c < self.width and state[r][hi_c] == 1:
 			hi_c += 1
 		hi_c -= 1
 
@@ -115,15 +123,15 @@ class Game:
 		hi_r = r
 		lo_c = c
 		hi_c = c
-		while state[lo_r][lo_c] == 1 and lo_r > -1 and lo_c > -1:
+		while lo_r > -1 and lo_c > -1 and state[lo_r][lo_c] == 1:
 			lo_r -= 1
 			lo_c -= 1
 		lo_r += 1
 		lo_c += 1
-		while state[hi_r][hi_c] == 1 and hi_r < self.height \
-				and hi_c < self.width:
+		while hi_r < self.height and hi_c < self.width and \
+				state[hi_r][hi_c] == 1:
 			hi_r += 1
-			h-_c += 1
+			hi_c += 1
 		hi_r -= 1
 		hi_c -= 1
 
@@ -134,13 +142,13 @@ class Game:
 		hi_r = r
 		lo_c = c
 		hi_c = c
-		while state[lo_r][lo_c] == 1 and lo_r > -1 and lo_c < self.width:
+		while lo_r > -1 and lo_c < self.width and state[lo_r][lo_c] == 1:
 			lo_r -= 1
 			lo_c += 1
 		lo_r += 1
 		lo_c -= 1
-		while state[hi_r][hi_c] == 1 and hi_r < self.height \
-				and hi_c > -1:
+		while hi_r < self.height and hi_c > -1 and \
+				state[hi_r][hi_c] == 1:
 			hi_r += 1
 			hi_c -= 1
 		hi_r -= 1
