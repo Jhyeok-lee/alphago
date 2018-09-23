ab33f03d1aa1499877fbdf339f6e7d2889fb1d1a
Jaehyeok Lee <wogur0310@gmail.com>
2018-09-23 13:47:14 +0900

    game : renewal

--- game.py
+++ game.py
@@ -1,312 +1,152 @@
-import random
 import numpy as np
-from ddogo import DDoGo
 
-class Sixmok:
-	def __init__(self, height, width, brain):
+class Game:
+	def __init__(self, height, width, max_state_size=7):
 		self.width = width
 		self.height = height
-		self.brain = brain
-		self.ddoggo = DDoGo(height, width, brain)
+		self.max_state_size = max_state_size
+		self.black_states = []
+		self.white_states = []
+		self.counts = height * width
 		self.reset()
 
-	def reset(self, pre_train=0):
-		self.player = random.randrange(1, 3)
-		self.state = np.zeros(self.width * self.height).reshape(
-			self.width, self.height)
-		self.available = np.array(self.state) + 1
-		self.randomBlockingCnt = random.randrange(0, 6) * 2
-		self.remain = self.width * self.height - self.randomBlockingCnt
-		self.blocking = []
-		self.gibo = []
-		self.pre_train = pre_train
+	def reset(self):
+		self.black_states.clear()
+		self.white_states.clear()
+		self.counts = self.height * self.width
+		for i in range(self.max_state_size):
+			zero_state_black = np.zeros((self.height, self.width))
+			zero_state_white = np.zeros((self.height, self.width))
+			black_states.append(zero_state_black)
+			white_states.append(zero_state_white)
 
-		for i in range(0, self.randomBlockingCnt):
-			x = random.randrange(0, self.height)
-			y = random.randrange(0, self.width)
-			if self.state[x][y] == 0:
-				self.state[x][y] = 3
-				self.available[x][y] = 0
-				self.blocking.append([x,y])
+	def play(self, black_player, white_player):
+		self.reset()
+		player = 1
+		players = [white_player, black_player]
+		game_states = []
+		action_probs = []
+		current_players = []
+		winner = -1
+		while True:
+			current_state = self.get_current_state(player)
+			action, probs = players[player].get_action(
+				current_state)
+
+			game_states.append(current_state)
+			action_probs.append(probs)
+			current_players.append(player)
+
+			winner = self.do_action(player, action)
+			if winner == -1:
+				player ^= 1
 			else:
-				i -= 1
+				break
 
-	def runSelfPlay(self):
-		turns = 0
-		winner = 0
-		states, action_probs, current_players = [], [], []
-		while winner == 0:
-			turns += 1
-			
-			if self.pre_train == 0:
-				if turns == 1 and self.randomBlockingCnt == 0:
-					action = int(self.height/2) * self.height + int(self.width/2)
-				else:
-					action = self.ddoggo.get_action(self.player, 
-							self.state, self.available)
-				prob = np.zeros(self.height * self.width)
-				prob[action] = 0.42
-			else:
-				action = self.lastAdjPolicy(self.player)
-				prob = np.zeros(self.height * self.width)
-				prob[action] = 0.42
+		if winner == 2:
+			return [], [], []
 
-			x = int(action / self.width)
-			y = action % self.height
-			states.append(np.array(self.state))
-			action_probs.append(prob)
-			current_players.append(self.player)
+		values = np.zeros(len(current_players))
+		values[np.array(current_players) == winner] = 1.0
+		values[np.array(current_players) != winner] = -1.0
 
-			self.state[x][y] = self.player
-			self.available[x][y] = 0
-			self.remain -= 1
-			self.gibo.append([x,y])
-			winner = self.checkFinish(self.player, x, y)
-			if self.player == 1:
-				self.player = 2
-			else:
-				self.player = 1
+		return game_states, action_probs, values
 
-		for i in range(len(states)):
-			if current_players[i] == 2:
-				states[i] = self.change_state(states[i])
+	def get_current_state(self, player):
+		ret = []
+		if player == 1:
+			ret.append(np.ones((height, width)))
+		else:
+			ret.append(np.zeros((height, width)))
 
-		winners = np.zeros(len(current_players))
-		winners[np.array(current_players) == winner] = 1.0
-		winners[np.array(current_players) != winner] = -1.0
-		return winner, turns, states, current_players, action_probs, winners, \
-			self.state, action
+		ret.extend(white_states[(len(white_states) - self.max_state_size):])
+		ret.extend(black_states[(len(black_states) - self.max_state_size):])
 
-	def runSelfPlay2(self):
-		turns = 0
-		winner = 0
-		states, action_probs, current_players = [], [], []
-		print(self.state)
-		while winner == 0:
-			turns += 1
-			
-			if self.player == 1:
-				action = self.ddoggo.get_action(self.player, 
-					self.state, self.available)
-				prob = np.zeros(self.height * self.width)
-				prob[action] = 0.42
-			else:
-				x = int(input('x : '))
-				y = int(input('y : '))
-				action = x * self.height + y
-				prob = np.zeros(self.height * self.width)
-				prob[action] = 0.42
+		return ret
 
-			x = int(action / self.width)
-			y = action % self.height
-			states.append(np.array(self.state))
-			action_probs.append(prob)
-			current_players.append(self.player)
+	def do_action(self, player, action):
+		r = int(action / self.height)
+		c = action % self.width
+		state = None
+		if player == 1:
+			state = np.array(self.black_states[len(self.black_states)-1])
+		else:
+			state = np.array(self.white_states[len(self.white_states)-1])
+		state[r][c] = 1
+		self.counts -= 1
+		if player == 1:
+			self.black_states.append(state)
+		else:
+			self.white_states.append(state)
 
-			self.state[x][y] = self.player
-			print(self.state)
-			self.available[x][y] = 0
-			self.remain -= 1
-			self.gibo.append([x,y])
-			winner = self.checkFinish(self.player, x, y)
-			if self.player == 1:
-				self.player = 2
-			else:
-				self.player = 1
-
-		for i in range(len(states)):
-			if current_players[i] == 2:
-				states[i] = self.change_state(states[i])
-
-		winners = np.zeros(len(current_players))
-		winners[np.array(current_players) == winner] = 1.0
-		winners[np.array(current_players) != winner] = -1.0
-		return winner, turns, states, current_players, action_probs, winners, \
-			self.state, action
-
-	def change_state(self, state):
-		ret_state = state
-		ret_state[ret_state == 2] = 4
-		ret_state[ret_state == 1] = 2
-		ret_state[ret_state == 4] = 1
-		return ret_state
-
-	def checkFinish(self, player, x, y):
-		# up-down
-		ylo = y
-		yhi = y
-		while ylo >= 0 and (self.state[x][ylo] == player or
-			self.state[x][ylo] == 3):
-			ylo -= 1
-		ylo += 1
-		while yhi < self.width and (self.state[x][yhi] == player or
-			self.state[x][yhi] == 3):
-			yhi += 1
-		yhi -= 1
-		if yhi - ylo + 1 == 6:
+		if self.is_win(state, r, c):
 			return player
-		if yhi - ylo + 1 > 6:
-			if player == 1:
-				return 2
-			return 1
 
-		# left-right
-		xlo = x
-		xhi = x
-		while xlo >= 0 and (self.state[xlo][y] == player or
-			self.state[xlo][y] == 3):
-			xlo -= 1
-		xlo += 1
-		while xhi < self.height and (self.state[xhi][y] == player or
-			self.state[xhi][y] == 3):
-			xhi += 1
-		xhi -= 1
-		if xhi - xlo + 1 == 6:
-			return player
-		if xhi - xlo + 1 > 6:
-			if player == 1:
-				return 2
-			return 1
+		if self.counts == 0:
+			return 2
 
-		# diagnal
-		xlo = x
-		ylo = y
-		xhi = x
-		yhi = y
-		while xlo >= 0 and ylo >= 0 and (self.state[xlo][ylo] == player or
-			self.state[xlo][ylo] == 3):
-			xlo -= 1
-			ylo -= 1
-		xlo += 1
-		ylo += 1
-		while xhi < self.height and yhi < self.width and (self.state[xhi][yhi] == player or
-			self.state[xhi][yhi] == 3):
-			xhi += 1
-			yhi += 1
-		xhi -= 1
-		yhi -= 1
-		if xhi - xlo + 1 == 6:
-			return player
-		if xhi - xlo + 1 > 6:
-			if player == 1:
-				return 2
-			return 1
+		return -1
 
-		xlo = x
-		ylo = y
-		xhi = x
-		yhi = y
-		while xlo >= 0 and ylo < self.width and (self.state[xlo][ylo] == player or
-			self.state[xlo][ylo] == 3):
-			xlo -= 1
-			ylo += 1
-		xlo += 1
-		ylo -= 1
-		while xhi < self.height and yhi >= 0 and (self.state[xhi][yhi] == player or
-			self.state[xhi][yhi] == 3):
-			xhi += 1
-			yhi -= 1
-		xhi -= 1
-		yhi += 1
-		if xhi - xlo + 1 == 6:
-			return player
-		if xhi - xlo + 1 > 6:
-			if player == 1:
-				return 2
-			return 1
+	def is_win(self, state, r, c):
+		lo_r = r
+		hi_r = r
+		while state[lo_r][c] == 1 and lo_r > -1:
+			lo_r -= 1
+		lo_r += 1
+		while state[hi_r][c] == 1 and hi_r < self.height:
+			hi_r += 1
+		hi_r -= 1
+		if hi_r - lo_r + 1 == 5:
+			return True
 
-		if self.remain <= 0:
-			return 3
+		lo_c = c
+		hi_c = c
+		while state[r][lo_c] == 1 and lo_c > -1:
+			lo_c -= 1
+		lo_c += 1
+		while state[r][hi_c] == 1 and hi_c < self.width:
+			hi_c += 1
+		hi_c -= 1
 
-		return 0
+		if hi_c - lo_c + 1 == 5:
+			return True
 
-	def checkBoard(self, x, y):
-		if x < 0 or x > self.height-1:
-			return False
-		if y < 0 or y > self.width-1:
-			return False
-		if self.state[x][y] != 0:
-			return False
-		return True
+		lo_r = r
+		hi_r = r
+		lo_c = c
+		hi_c = c
+		while state[lo_r][lo_c] == 1 and lo_r > -1 and lo_c > -1:
+			lo_r -= 1
+			lo_c -= 1
+		lo_r += 1
+		lo_c += 1
+		while state[hi_r][hi_c] == 1 and hi_r < self.height \
+				and hi_c < self.width:
+			hi_r += 1
+			h-_c += 1
+		hi_r -= 1
+		hi_c -= 1
 
-	def randomPolicy(self, player):
-		if self.remain <= 0:
-			return 0
+		if hi_r - lo_r + 1 == 5:
+			return True
 
-		x = random.randrange(0, self.height)
-		y = random.randrange(0, self.width)
-		while self.state[x][y] != 0:
-			x = random.randrange(0, self.height)
-			y = random.randrange(0, self.width)
+		lo_r = r
+		hi_r = r
+		lo_c = c
+		hi_c = c
+		while state[lo_r][lo_c] == 1 and lo_r > -1 and lo_c < self.width:
+			lo_r -= 1
+			lo_c += 1
+		lo_r += 1
+		lo_c -= 1
+		while state[hi_r][hi_c] == 1 and hi_r < self.height \
+				and hi_c > -1:
+			hi_r += 1
+			hi_c -= 1
+		hi_r -= 1
+		hi_c += 1
 
-		return x * self.height + y
+		if hi_r - lo_r + 1 == 5:
+			return True
 
-	def adjacentPolicy(self, player):
-		if self.randomBlockingCnt == 0:
-			return self.randomPolicy(player)
-		i = player-1
-		adj = [[-1,-1], [-1,0], [-1,1], [0,-1], \
-				[0,1], [1,-1], [1,0], [1,1]]
-		candidate = []
-		tempState = np.array(self.state)
-		while i < len(self.gibo):
-			for k in adj:
-				x = self.gibo[i][0] + k[0]
-				y = self.gibo[i][1] + k[1]
-				if self.checkBoard(x,y) == False:
-					continue
-
-				tempState[x][y] = player
-				winner = self.checkFinish(player, x, y)
-				if winner == 0 or winner == player:
-					candidate.append([x,y])
-				tempState[x][y] = 0
-
-			i += 2
-
-		tempState = np.array(self.state)
-		for i in self.blocking:
-			for j in adj:
-				x = i[0] + j[0]
-				y = i[1] + j[1]
-
-				if self.checkBoard(x,y) == False:
-					continue
-				
-				tempState[x][y] = player
-				winner = self.checkFinish(player, x, y)
-				if winner == 0 or winner == player:
-					candidate.append([x,y])
-				tempState[x][y] = 0
-
-		if len(candidate) == 0:
-			return self.randomPolicy(player)
-
-		n = random.randrange(0, len(candidate))
-		return candidate[n][0] * self.height + candidate[n][1]
-
-	def lastAdjPolicy(self, player):
-		if len(self.gibo) == 0:
-			return self.adjacentPolicy(player)
-
-		tempState = self.state
-		candidate = []
-		adj = [[-1,-1], [-1,0], [-1,1], [0,-1], \
-				[0,1], [1,-1], [1,0], [1,1]]
-		for i in adj:
-			x = self.gibo[len(self.gibo)-2][0] + i[0]
-			y = self.gibo[len(self.gibo)-2][1] + i[1]
-			if self.checkBoard(x,y) == False:
-				continue
-
-			tempState[x][y] = player
-			winner = self.checkFinish(player, x, y)
-			if winner == 0 or winner == player:
-				candidate.append([x,y])
-			tempState[x][y] = 0
-
-		if len(candidate) == 0:
-			return self.adjacentPolicy(player)
-
-		n = random.randrange(0, len(candidate))
-		return candidate[n][0] * self.height + candidate[n][1]
+		return False
